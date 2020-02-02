91663c7d985190009f9ca73534bfcc3a293bdc7f
krsinghshubham <47324260+krsinghshubham@users.noreply.github.com>
2020-02-02 21:10:53 +0530

    update

--- personal_expenses_2nd_app/ios/Flutter/flutter_export_environment.sh
+++ personal_expenses_2nd_app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\padhai\projects\personal_expenses_2nd_app"
+export "FLUTTER_APPLICATION_PATH=D:\Imp\projects\Flutter-apps\personal_expenses_2nd_app"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
